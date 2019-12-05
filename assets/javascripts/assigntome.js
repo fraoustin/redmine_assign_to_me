ece90fb4390d4294827e2e47c7519ed120d29749
fraoustin <fraoustin@gmail.com>
2019-12-05 15:00:09 +0100

    <feat> assign: add js function

--- assets/javascripts/assigntome.js
+++ assets/javascripts/assigntome.js
@@ -1,1 +1,10 @@
-console.log("assignToMe")
+$(document).ready(function() {
+    console.log("assignToMe");
+    Array.from(document.querySelectorAll('#issue_assigned_to_id')).forEach(elt => {
+        if (elt.value == "") {
+            elt.value = elt.querySelectorAll('option:not([value=""])')[0].getAttribute('value');
+        }
+    })
+});
+ 
+
